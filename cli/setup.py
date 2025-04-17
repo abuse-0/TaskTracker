08a0a22fb1be12b7884bbb132a83234c776a5dc0
abuse-0 <kolkovzena03@gmail.com>
2025-04-17 19:13:00 +0500

    done

--- cli/setup.py
+++ cli/setup.py
@@ -4,8 +4,7 @@ setup(
     py_modules=["main", "task_manager"], # files
     entry_points={
         "console_scripts": [
-            "task-cli=main:main", # yupiyo = main.py:function main();
-           # yupiyo = python3 main.py 
+            "task-cli=main:main", # task-cli = main.py:function main();
         ],
     },
 )
